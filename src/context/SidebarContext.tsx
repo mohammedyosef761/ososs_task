5d2f00f2ae65c1df90cd3bb7b2951c6a64485356
mohammed <mohammedyosef712@gmail.com>
2023-10-16 11:15:11 -0700

    finishing from the Ososs task

--- src/context/SidebarContext.tsx
+++ src/context/SidebarContext.tsx
@@ -1,36 +1,36 @@
-
-
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState } from "react";
 
 interface SidebarContextProps {
-    isSidebarOpen: boolean;
-    toggleSidebar: () => void;
+  isSidebarOpen: boolean;
+  toggleSidebar: () => void;
 }
 
-const SidebarContext = createContext<SidebarContextProps | undefined>(undefined);
+const SidebarContext = createContext<SidebarContextProps | undefined>(
+  undefined
+);
 
 export const useSidebarContext = () => {
-    const context = useContext(SidebarContext);
-    if (!context) {
-        throw new Error('useSidebarContext must be used within a SidebarProvider');
-    }
-    return context;
+  const context = useContext(SidebarContext);
+  if (!context) {
+    throw new Error("there is an error in the sidebar context");
+  }
+  return context;
 };
 
 type Props = {
-    children: React.ReactNode;
-}
+  children: React.ReactNode;
+};
 
 export const SidebarProvider: React.FC<Props> = ({ children }) => {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-    const toggleSidebar = React.useCallback(() => {
-        setIsSidebarOpen(prev => !prev);
-    }, []);
+  const toggleSidebar = React.useCallback(() => {
+    setIsSidebarOpen((prev) => !prev);
+  }, []);
 
-    return (
-        <SidebarContext.Provider value={{ isSidebarOpen, toggleSidebar }}>
-            {children}
-        </SidebarContext.Provider>
-    );
+  return (
+    <SidebarContext.Provider value={{ isSidebarOpen, toggleSidebar }}>
+      {children}
+    </SidebarContext.Provider>
+  );
 };
